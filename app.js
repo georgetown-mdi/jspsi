fb8cb691da5a96760c84f13cd728d674b3e047ee
Vincent Dorie <vdorie@gmail.com>
2025-07-05 11:32:42 -0400

    use NODE_ENV

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -14,6 +13,8 @@ var app = module.exports = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.set('query parser', (str) => qs.parse(str))
+
+app.set('isDevelopment', process.env.NODE_ENV === 'development');
 
 app.use(logger('dev'));
 app.use(express.json());
