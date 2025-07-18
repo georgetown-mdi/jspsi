24d05c6fd383e5aadd6f124815f19db7410a5730
Vincent Dorie <vdorie@gmail.com>
2025-07-18 11:48:00 -0700

    add copy button

--- src/routes/psi/start.tsx
+++ src/routes/psi/start.tsx
@@ -1,6 +1,7 @@
 
 import { createFileRoute } from '@tanstack/react-router';
-import { Center, Paper, Title, Text, Stack } from '@mantine/core';
+import { Center, Paper, Title, Text, Stack, Button } from '@mantine/core';
+import { useClipboard } from '@mantine/hooks';
 
 import type { Session } from '../../utils/sessions';
 
@@ -13,7 +14,7 @@ export const Route = createFileRoute('/psi/start')({
   },
   loaderDeps: ({ search: { id } }) => ({ id }),
   loader: async ({ deps: { id } }) =>  {
-    const response = await fetch(`../api/psi/${id}`)
+    const response = await fetch(`/api/psi/${id}`)
       if (!response.ok) {
         throw new Error(`failed to lookup PSI with id ${id} with error: ${response.statusText}`);
       }
@@ -25,7 +26,9 @@ export const Route = createFileRoute('/psi/start')({
 function Home() {
   const { id } = Route.useSearch();
   const session = Route.useLoaderData();
+  const clipboard = useClipboard();
 
+  // TODO: figure out how to detect if on server and user that when applicable
   const url = `${window.location.protocol}//${window.location.host}/api/psi/${id}/join`;
   
   return (
@@ -45,6 +48,11 @@ function Home() {
         <Paper shadow="xs" p="xl">
           <Title order={1}>Sharable Link</Title>
           <pre>{url}</pre>
+          <Button
+            color={clipboard.copied ? 'teal' : 'blue'}
+            onClick={() => clipboard.copy(url)}
+          >{clipboard.copied ? 'Copied' : 'Copy'}
+          </Button>
         </Paper>
       </Stack>
     </Center>
