a97abcc2e7474cec6140787766a4d777d0c5787c
Vincent Dorie <vdorie@gmail.com>
2025-07-24 15:48:56 -0700

    add resolve paths to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
+import path from 'node:path';
+
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import viteReact from '@vitejs/plugin-react'
 
+
 export default defineConfig({
+   resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@util": path.resolve(__dirname, "src/util"),
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   server: {
     host: '127.0.0.1',
     port: 3000,
   },
   plugins: [
-    tsConfigPaths({
-      projects: ['./tsconfig.json']
-    }),
+    tsConfigPaths(),
     tanstackStart({ customViteReactPlugin: true, target: 'netlify' }),
     viteReact()
   ],
